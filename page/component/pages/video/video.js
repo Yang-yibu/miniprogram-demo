0cd395837542cb474cdf77eb451e18dc60f9c80c
jrf <yyn@126mail.cn>
2018-11-06 14:35:43 +0800

    测试 video 1

--- page/component/pages/video/video.js
+++ page/component/pages/video/video.js
@@ -1,7 +1,8 @@
 function getRandomColor() {
   const rgb = []
   for (let i = 0; i < 3; ++i) {
-    let color = Math.floor(Math.random() * 256).toString(16)
+    let color = Math.floor(Math.random() * 256)
+      .toString(16)
     color = color.length === 1 ? '0' + color : color
     rgb.push(color)
   }
@@ -12,7 +13,7 @@ Page({
   onShareAppMessage() {
     return {
       title: 'video',
-      path: 'page/component/pages/video/video'
+      path: 'page/component/pages/video/video',
     }
   },
 
@@ -22,17 +23,17 @@ Page({
 
   inputValue: '',
   data: {
-    src: '',
+    src: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
     danmuList:
-    [{
-      text: '第 1s 出现的弹幕',
-      color: '#ff0000',
-      time: 1
-    }, {
-      text: '第 3s 出现的弹幕',
-      color: '#ff00ff',
-      time: 3
-    }]
+      [{
+        text: '第 1s 出现的弹幕',
+        color: '#ff0000',
+        time: 1,
+      }, {
+        text: '第 3s 出现的弹幕',
+        color: '#ff00ff',
+        time: 3,
+      }],
   },
 
   bindInputBlur(e) {
@@ -47,21 +48,21 @@ Page({
       camera: ['front', 'back'],
       success(res) {
         that.setData({
-          src: res.tempFilePath
+          src: res.tempFilePath,
         })
-      }
+      },
     })
   },
 
   bindSendDanmu() {
     this.videoContext.sendDanmu({
       text: this.inputValue,
-      color: getRandomColor()
+      color: getRandomColor(),
     })
   },
 
   videoErrorCallback(e) {
     console.log('视频错误信息:')
     console.log(e.detail.errMsg)
-  }
+  },
 })
